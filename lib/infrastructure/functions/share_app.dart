2f5af9041837058783c3bb90364c6783cc404ed1
siyadas878 <siyadas878@gmail.com>
2023-09-21 09:04:20 +0530

    key added

--- lib/infrastructure/functions/share_app.dart
+++ lib/infrastructure/functions/share_app.dart
@@ -3,6 +3,6 @@ import 'package:share/share.dart';
 void shareApp() {
   String musicTitle = "Mr.musik";
   String musicUrl =
-      "https://play.google.com/store/apps/details?id=com.brototype.mr_musik";
+      "https://play.google.com/store/apps/details?id=com.example.hotspot";
   Share.share("Check out this amazing music player App: $musicTitle\n\n$musicUrl");
 }
